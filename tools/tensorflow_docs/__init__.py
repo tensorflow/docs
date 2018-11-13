20273d977d9dd4370da0450d63af1dcd3c158d2e
Mark Daoust <markdaoust@google.com>
2018-11-12 17:06:36 -0800

    Add standard futures.

--- tools/tensorflow_docs/__init__.py
+++ tools/tensorflow_docs/__init__.py
@@ -1,4 +1,7 @@
 """tensorflow_docs is a package for generating python api-reference docs."""
 
-from tensorflow_docs import api_generator
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+from tensorflow_docs import api_generator
