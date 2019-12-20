5d91a2e1ad9e1335e86afa7aaa248eb39ef97935
Yash Katariya <yashkatariya@google.com>
2019-12-20 14:18:35 -0800

    Fix the _redirects for tf_overview for versions.
    PiperOrigin-RevId: 286639269

--- tools/tensorflow_docs/api_generator/generate_lib.py
+++ tools/tensorflow_docs/api_generator/generate_lib.py
@@ -542,6 +542,12 @@ def write_docs(output_dir,
       })
 
   if redirects:
+    if yaml_toc:
+      redirects.append({
+          'from': os.path.join('/', site_path, 'tf_overview'),
+          'to': os.path.join('/', site_path, 'tf')
+      })
+
     redirects_dict = {
         'redirects': sorted(redirects, key=lambda redirect: redirect['from'])
     }
@@ -555,12 +561,12 @@ def write_docs(output_dir,
     toc_dict = toc_gen.generate()
 
     # Replace the overview path *only* for 'TensorFlow' to
-    # `/api_docs/tf_overview`. This will be redirected to `/api_docs/python/tf`.
+    # `/api_docs/python/tf_overview`. This will be redirected to
+    # `/api_docs/python/tf`.
     toc_values = toc_dict['toc'][0]
     if toc_values['title'] == 'tf':
       section = toc_values['section'][0]
-      if '/versions/' not in section['path']:
-        section['path'] = '/api_docs/tf_overview'
+      section['path'] = os.path.join('/', site_path, 'tf_overview')
 
     leftnav_toc = os.path.join(output_dir, '_toc.yaml')
     with open(leftnav_toc, 'w') as toc_file:
