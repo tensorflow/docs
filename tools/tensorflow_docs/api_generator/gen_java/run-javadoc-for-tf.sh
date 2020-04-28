0d4b376f2b6c44866497d3cacaaba642421d5c00
Mark Daoust <markdaoust@google.com>
2020-04-28 07:37:05 -0700

    Remove reference/ directory in java-api paths
    paths will be `api_docs/java/org` instead of `api_docs/java/reference/org`.
    
    PiperOrigin-RevId: 308820351

--- tools/tensorflow_docs/api_generator/gen_java/run-javadoc-for-tf.sh
+++ tools/tensorflow_docs/api_generator/gen_java/run-javadoc-for-tf.sh
@@ -6,19 +6,19 @@ set -ex
 # https://packages.debian.org/unstable/doclava-aosp
 # Install with:
 #
-#   $ sudo apt install doclava-aosp
+#   $ sudo apt install doclava-aosp #v 6.0.1+r55-1+build1
 DOCLAVA_JAR=/usr/share/java/doclava.jar
 
 # Install java clear silver templates with:
 #
-#   $ sudo apt install libjsilver-aosp-java
+#   $ sudo apt install libjsilver-aosp-java #v 6.0.1+r55-1+build1
 JSILVER_JAR=/usr/share/java/jsilver.jar
 
 
 ######### DELETE OUTPUT_DIR #################
 
 # Delete the output directory in case a class has been deleted
-rm -rf "${OUTPUT_DIR}/reference"
+rm -rf "${OUTPUT_DIR}/org"
 
 ############ RUN DOCLAVA ###################
 
@@ -48,13 +48,17 @@ rm -rf "${OUTPUT_DIR}/reference"
 )
 
 
+mv "${OUTPUT_DIR}"/reference/* "${OUTPUT_DIR}"
+
 ###################################################################
 ################### START OF POST-PROCESSING ######################
 ###################################################################
 rm "${OUTPUT_DIR}/navtree_data.js"
-rm "${OUTPUT_DIR}/reference/hierarchy.html"
+rm "${OUTPUT_DIR}/hierarchy.html"
 
-find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place "s|a href=\"reference/org/tensorflow|a href=\"${SITE_PATH}/reference/org/tensorflow|g"
+find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place "s|${SITE_PATH}/reference|${SITE_PATH}|g"
+find ${OUTPUT_DIR} -name "*.yaml" | xargs sed --in-place "s|${SITE_PATH}/reference|${SITE_PATH}|g"
+find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place "s|a href=\"reference/org/tensorflow|a href=\"${SITE_PATH}/org/tensorflow|g"
 
 JAVA_LANG=https://docs.oracle.com/javase/8/docs/api
 find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place "s|a href=\"reference/java/lang|a href=\"${JAVA_LANG}/java/lang|g"
@@ -62,9 +66,9 @@ find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place "s|a href=\"reference/j
 find ${OUTPUT_DIR} -name "*.html" | xargs sed --in-place 's|<pre><code>|<pre class="prettyprint"><code>|g'
 
 rm ${OUTPUT_DIR}/timestamp.js
-rm ${OUTPUT_DIR}/reference/lists.js
-rm ${OUTPUT_DIR}/reference/index.html
+rm ${OUTPUT_DIR}/lists.js
+rm ${OUTPUT_DIR}/index.html
 
-cp ${TEMPLATES}/screen.css ${OUTPUT_DIR}/reference/
+cp ${TEMPLATES}/screen.css ${OUTPUT_DIR}/
 
 
