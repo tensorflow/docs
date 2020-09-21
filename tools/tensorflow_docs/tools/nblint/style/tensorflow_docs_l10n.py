b4a447a682a9b8973ded42e6d283c7c5dddb366e
Billy Lamberta <blamb@google.com>
2020-09-21 10:28:17 -0700

    Add lint check that RTL languages contain a div directional wrapper.
    PiperOrigin-RevId: 332879588

--- tools/tensorflow_docs/tools/nblint/style/tensorflow_docs_l10n.py
+++ tools/tensorflow_docs/tools/nblint/style/tensorflow_docs_l10n.py
@@ -34,6 +34,7 @@ from tensorflow_docs.tools.nblint import fix
 from tensorflow_docs.tools.nblint.decorator import fail
 from tensorflow_docs.tools.nblint.decorator import lint
 from tensorflow_docs.tools.nblint.decorator import Options
+from tensorflow_docs.tools.nblint.style.tensorflow import is_button_cell_re
 from tensorflow_docs.tools.nblint.style.tensorflow import split_doc_path
 
 
@@ -79,12 +80,54 @@ def china_hostname_url(args):
   """
   docs_dir, _ = split_doc_path(args["path"])
 
-  if str(docs_dir) == "site/zh-cn" or str(docs_dir) == "site/zh-tw":
-    if has_tf_hostname_re.search(args["cell_source"]):
-      fail(
-          fix=fix.regex_replace_all,
-          fix_args=[has_tf_hostname_re.pattern, "tensorflow.google.cn"])
-    else:
-      return True
+  # Only applicable for China docs.
+  if str(docs_dir) != "site/zh-cn" and str(docs_dir) != "site/zh-tw":
+    return True
+
+  if has_tf_hostname_re.search(args["cell_source"]):
+    fail(
+        fix=fix.regex_replace_all,
+        fix_args=[has_tf_hostname_re.pattern, "tensorflow.google.cn"])
   else:
     return True
+
+
+has_rtl_div_re = re.compile(r"<div\s*dir\s*=\s*[\"']rtl[\"'].*>", re.IGNORECASE)
+has_copyright_re = re.compile(r"Copyright 20[1-9][0-9]")
+
+
+@lint(
+    message="RTL languages must wrap all text cell elements with: <div dir=\"rtl\">...</div>",
+    scope=Options.Scope.TEXT,
+    cond=Options.Cond.ALL)
+def rtl_language_wrap(args):
+  """Check that RTL languages wrap text elemenst in a directional div.
+
+  Required for languages like Arabic to render correctly in Colab. Some care
+  must be taken or any Markdown syntax within the div will break.
+
+  Args:
+    args: Nested dict of runtime arguments.
+
+  Returns:
+    Boolean: True if lint test passes, False if not.
+  """
+  docs_dir, _ = split_doc_path(args["path"])
+
+  # Only applicable for RTL languages.
+  if str(docs_dir) != "site/ar":
+    return True
+
+  cell_source = args["cell_source"]
+
+  # Ignore the text cells for copyright and buttons.
+  if (has_copyright_re.search(cell_source) or
+      is_button_cell_re.search(cell_source)):
+    return True
+
+  if has_rtl_div_re.search(cell_source):
+    return True
+  else:
+    fail(
+        "Wrap all text elements in `<div dir=\"rtl\">...</div>` for Colab. But check this doesn't break any Markdown syntax within."
+    )
