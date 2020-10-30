fda9abaae54173bfeed3c0c57a5b5309717ea5ca
Yash Katariya <yashkatariya@google.com>
2020-10-30 13:35:39 -0700

    Check for Yields too for the api report.
    PiperOrigin-RevId: 339931023

--- tools/tensorflow_docs/api_generator/report/linter.py
+++ tools/tensorflow_docs/api_generator/report/linter.py
@@ -153,10 +153,10 @@ class ReturnVisitor(ast.NodeVisitor):
 
 def lint_returns(
     page_info: parser.PageInfo) -> Optional[api_report_pb2.ReturnLint]:
-  """"Lints the returns block in the docstring.
+  """"Lints the returns/yields block in the docstring.
 
-  This linter only checks if a `Returns` block exists in the docstring
-  if it finds `return` keyword in the source code.
+  This linter only checks if a `Returns`/`Yields` block exists in the docstring
+  if it finds `return`/`yield` keyword in the source code.
 
   Args:
     page_info: A `PageInfo` object containing the information of a page
@@ -174,14 +174,16 @@ def lint_returns(
     except Exception:  # pylint: disable=broad-except
       pass
 
-  if source is not None and 'return' in source:
+  keywords = ('return', 'yield')
+
+  if source is not None and any(word in source for word in keywords):
     for item in page_info.doc.docstring_parts:
       if isinstance(item, parser.TitleBlock):
-        if item.title.lower().startswith('return'):
+        if item.title.lower().startswith(keywords):
           return api_report_pb2.ReturnLint(returns_defined=True)
-    # If "Returns" word is present in the brief docstring then having a separate
-    # `Returns` section is not needed.
-    if 'return' in page_info.doc.brief.lower():
+    # If "Returns"/"Yields" word is present in the brief docstring then having
+    # a separate `Returns`/`Yields` section is not needed.
+    if page_info.doc.brief.lower().startswith(keywords):
       return api_report_pb2.ReturnLint(returns_defined=True)
     # If the code only returns None then `Returns` section in the docstring is
     # not required.
