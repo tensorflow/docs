8325ca7de50d10c1dacbb28b1de4f3ab8091420b
A. Unique TensorFlower <gardener@tensorflow.org>
2021-12-08 05:00:15 -0800

    Fix error when py_object has custom `__eq__`
    PiperOrigin-RevId: 414969859

--- tools/tensorflow_docs/api_generator/doc_generator_visitor.py
+++ tools/tensorflow_docs/api_generator/doc_generator_visitor.py
@@ -45,7 +45,7 @@ def maybe_singleton(py_object: Any) -> bool:
   is_immutable_type = isinstance(py_object, immutable_types)
 
   # Check if the object is the empty tuple.
-  return is_immutable_type or py_object == ()
+  return is_immutable_type or (isinstance(py_object, tuple) and py_object == ())  # pylint: disable=g-explicit-bool-comparison
 
 
 class ApiTreeNode(object):
